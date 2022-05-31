9ef665ff1c8fbc4675591d728de46071f48bb094
zooyaho <jiwoo1268@naver.com>
2022-05-31 19:53:36 +0900

    구별된 유니언을 사용하여 유니언 타입인 객체를 구별하여 해당 속성을 사용함.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 // intesection type: &, 두 객체타입을 결합시키거나 두 유니언타입의 공통타입으로 새로운 타입을 만듬.
 // 타입가드: 유니온타입이 지닌 유연성을 활용할 수 있게 해주며, 런타임 시 코드가 정확히 작동하게 해줌. 특정 속성이나 메서드를 사용하기 전에 그것이 존재하는지 확인하거나 타입을 사용하기 전에 이 타입으로 어떤 작업을 수행할 수 있는지를 확인하는 개념 또는 방식을 말함!!, 객체의 경우 in,instanceof를 사용하고 다른 타입들의 경우 typeof를 사용할 수 있음.
-// 구별된 유니언: 타입가드를 쉽게 구현할 수 있게 해줌, 객체 타입 사용시 유용
+// 구별된 유니언: 타입가드를 쉽게 구현할 수 있게 해줌, 객체 타입 사용시 유용, 유니언을 구성하는 모든 객체에 하나의 공통속성만 있고, switch문을 사용하여 공통속성에 따른 객체를 구별하여 해당 속성을 사용함
 
 type Admin = {
   name: string;
@@ -42,11 +42,11 @@ type UnknwonEmployee = Admin | Employee;
 
 function printEmployeeInformation(emp: UnknwonEmployee) {
   console.log(emp.name);
-  if ('privileges' in emp) {
+  if ("privileges" in emp) {
     // in으로 privileges이 emp객체의 속성으로 있는지 알수있음.
     console.log(emp.privileges);
   }
-  if ('startDate' in emp) {
+  if ("startDate" in emp) {
     console.log(emp.startDate);
   }
 }
@@ -54,16 +54,16 @@ printEmployeeInformation(e1);
 
 // class 에서의 타입가드 -> instanceof
 class Car {
-  drive(){
-    console.log('Driving~!');
+  drive() {
+    console.log("Driving~!");
   }
 }
 class Truck {
-  drive(){
-    console.log('Driving a Truck~!');
+  drive() {
+    console.log("Driving a Truck~!");
   }
-  loadCargo(amount:number){
-    console.log('Loading Cargo ' + amount);
+  loadCargo(amount: number) {
+    console.log("Loading Cargo " + amount);
   }
 }
 
@@ -71,10 +71,10 @@ type Vehicle = Car | Truck;
 const v1 = new Car();
 const v2 = new Truck();
 
-function useVehicle(vehicle:Vehicle){
+function useVehicle(vehicle: Vehicle) {
   vehicle.drive();
   // if('loadCargo' in vehicle){
-  if(vehicle instanceof Truck){
+  if (vehicle instanceof Truck) {
     // vehicle이 Truck의 인스턴스인지 확인함.
     // instanceof는 자바스크립트에 내장되어 있음.
     vehicle.loadCargo(1000);
@@ -82,3 +82,32 @@ function useVehicle(vehicle:Vehicle){
 }
 useVehicle(v1);
 useVehicle(v2);
+
+/* 구별된 유니언 */
+interface Bird {
+  type: "bird"; // 구별된 유니언
+  flyingSpeed: number;
+}
+interface Horse {
+  type: "horse"; // 구별된 유니언
+  runningSpeed: number;
+}
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  // if('flyingSpeed' in animal){
+  //   // interface타입이여서 instanceof로 타입가드 작성할 수 없음.
+  //   console.log("Moving with speed: " + animal.flyingSpeed);
+  // }
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+      break;
+  }
+  console.log(animal.type + " Moving at Speed: " + speed);
+}
+moveAnimal({ type: "bird", flyingSpeed: 300 });
